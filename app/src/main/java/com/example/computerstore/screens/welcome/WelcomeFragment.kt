32121c47aba69d0dc9dc5462af4cc596eab8f45e
Celia Dehors <celia.dehors@gmail.com>
2022-07-30 11:05:46 +0200

    Create welcome screen

---
+++ app/src/main/java/com/example/computerstore/screens/welcome/WelcomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.computerstore.screens.welcome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.computerstore.R
+import com.example.computerstore.databinding.WelcomeFragmentBinding
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: WelcomeFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.welcome_fragment,
+            container,
+            false
+        )
+        return binding.root
+    }
+}
