1cd90754585880cce91abfd5d57abd149e0af911
Celia Dehors <celia.dehors@gmail.com>
2022-07-31 17:51:09 +0200

    Create computer list screen

---
+++ app/src/main/java/com/example/computerstore/screens/list/ListFragment.kt
@@ -0,0 +1,37 @@
+package com.example.computerstore.screens.list
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.computerstore.R
+import com.example.computerstore.databinding.ListFragmentBinding
+
+class ListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: ListFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.list_fragment,
+            container,
+            false
+        )
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.logout_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+}
